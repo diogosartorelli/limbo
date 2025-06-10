162445d52b5cb8266a6c08eefbfd859f5dde66db
Diogo <diogosartorelli04@gmail.com>
2025-06-10 20:55:20 -0300

    Versão final com executavel

---
+++ build/exe.win-amd64-3.13/assets/funcoes.py
@@ -0,0 +1,48 @@
+import os, time
+import json
+from datetime import datetime
+
+
+def limpar_tela():
+    os.system("cls")
+    
+def aguarde(segundos):
+    time.sleep(segundos)
+    
+def inicializarBancoDeDados():
+    # r - read, w - write, a - append
+    try:
+        banco = open("base.atitus","r")
+    except:
+        print("Banco de Dados Inexistente. Criando...")
+        banco = open("base.atitus","w")
+    
+def escreverDados(nome, pontos):
+    arquivo = "log.dat"
+    dados = {}
+
+    # Carrega os dados existentes (se houver)
+    if os.path.exists(arquivo):
+        with open(arquivo, "r") as f:
+            try:
+                dados = json.load(f)
+            except json.JSONDecodeError:
+                dados = {}
+
+    # Obtém data e hora atual
+    agora = datetime.now()
+    data = agora.strftime("%d/%m/%Y")
+    hora = agora.strftime("%H:%M:%S")
+
+    # Cria a lista de partidas se ainda não existir
+    if nome not in dados:
+        dados[nome] = []
+
+    # Adiciona o novo registro (pontuação, data, hora)
+    dados[nome].append([pontos, data, hora])
+
+    # Salva de volta no arquivo
+    with open(arquivo, "w") as f:
+        json.dump(dados, f, indent=4)
+    
+    # END - inserindo no arquivo
